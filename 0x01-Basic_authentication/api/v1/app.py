5087d207ada2ef01c2057595931f4197641f5ae0
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-07-10 14:56:20 +0000

    task1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -9,7 +9,6 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -21,11 +20,11 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorized(error):
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
-
 
 
 if __name__ == "__main__":
