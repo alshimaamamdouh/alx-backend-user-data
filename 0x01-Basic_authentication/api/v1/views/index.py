0450ec0d6fde51f5f7647b3181fb844f41d075a7
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-07-10 18:02:58 +0300

    task2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,7 +33,6 @@ def unauthorized() -> str:
       - raises a 401 error by using abort
     """
     abort(401)
-<<<<<<< HEAD
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
@@ -42,5 +41,3 @@ def forbidden() -> str:
       - raises a 403 error by using abort
     """
     abort(403)
-=======
->>>>>>> 5087d207ada2ef01c2057595931f4197641f5ae0
